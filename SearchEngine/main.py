97bf9fd5312f889894cc63713aaa2c420d091b25
FedoraSukant <guptasukant2@gmail.com>
2024-06-08 11:55:02 +0530

    Changed BS4 code to test

--- SearchEngine/main.py
+++ SearchEngine/main.py
@@ -1,24 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_website(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Extract relevant data (title, content, etc.)
-    data = []
-    for page in soup.find_all('a', href=True):  # Example: finding all links
-        page_url = page['href']
-        page_title = page.get_text()
-        page_content = scrape_page_content(page_url)  # Implement this function to scrape content
-        data.append({'title': page_title, 'content': page_content, 'url': page_url})
-    return data
-
 def scrape_page_content(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    # Extract the main content of the page
-    content = soup.get_text()
+    content = soup.get_text()  # Get all the text from the page
     return content
 
+def scrape_website(base_url):
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    data = []
+    for link in soup.find_all('a', href=True):
+        page_url = link['href']
+        if not page_url.startswith('http'):
+            page_url = base_url + page_url  # Construct full URL if relative
+        page_title = link.get_text()
+        page_content = scrape_page_content(page_url)
+        data.append({'title': page_title, 'content': page_content, 'url': page_url})
+    return data
+
 # Example URL
-website_data = scrape_website('https://ohsl.us/projects/bcda')
+website_url = 'http://example.com'
+website_data = scrape_website(website_url)
+
+# Check the data
+for item in website_data:
+    print(f"Title: {item['title']}\nURL: {item['url']}\nContent: {item['content'][:100]}...\n")
